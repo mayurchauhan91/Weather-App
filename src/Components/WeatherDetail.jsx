a5818ab7b874010ecb5e47ba21630a93ed0fbd1b
Mayur Chauhan <mayurchauhan@Mayurs-MacBook-Pro.local>
2022-04-03 18:56:01 +0100

    bug fix

--- src/Components/WeatherDetail.jsx
+++ src/Components/WeatherDetail.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "./App.css";
+import Image from "../image/cloudy-day.png";
 
 const WeatherDetail = (props) => {
   return (
     <div className="col-md-12 result-container ">
       <div className="shadow rounded weatherResult">
-        <img
-          className="weather-img"
-          src="https://cdn-icons.flaticon.com/png/512/3751/premium/3751173.png?token=exp=1648999706~hmac=d86d0f8e0780e4bac6af504411af959d"
-          alt=""
-        />
+        <img className="weather-img" src={Image} alt="" />
         <h3 className="weather-city">{props.name}</h3>
         <h5 className="temp">
           {props.temperature}°<span className="temp-c">C</span>
