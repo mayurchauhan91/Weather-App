df63183343575fddf1ad1eaa4204588164399379
Mayur Chauhan <89522266+mayurchauhan91@users.noreply.github.com>
2022-05-04 12:48:08 +0100

    code update

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { makeStyles } from "@mui/styles";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
